c8b39008f6bd3a732cb74b4364a45df813febb2b
Tanni-Dey <tonnidey277615@gmail.com>
2022-05-02 23:01:19 +0600

    singlebook-api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -27,6 +27,15 @@ async function run() {
             const allBooks = await cursor.toArray();
             res.send(allBooks);
         })
+
+        //load single book with id
+        app.get('/book/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const oneBook = await bookCollection.findOne(query)
+            res.send(oneBook)
+        })
+
     }
 
     finally {
