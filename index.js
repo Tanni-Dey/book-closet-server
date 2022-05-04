d6ffdacfbf027193c9dbd5ccdf92b92a552567ba
Tanni-Dey <tonnidey277615@gmail.com>
2022-05-04 23:17:51 +0600

    deleteapi

--- index.js
+++ index.js
@@ -57,6 +57,15 @@ async function run() {
             res.send(addBook)
         })
 
+        //delete item api
+        app.delete('/book/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+
+            const deleteBook = await bookCollection.deleteOne(query);
+            res.send(deleteBook);
+        })
+
     }
 
     finally {
