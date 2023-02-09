1fca6845c987d4424742702334451946c40d45ff
Tanni-Dey <tonnidey277615@gmail.com>
2023-02-09 18:50:30 +0600

    new

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ function verifyJwt(req, res, next) {
 }
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uxezt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://bookUser:4IteTdDyYM260oE1@cluster0.uxezt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uxezt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://bookUser:4IteTdDyYM260oE1@cluster0.uxezt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
